24ee3e8abe488d0620de6f0b6d08b8bd46b7a39d
piotrpenza <ppenza@gmail.com>
2021-12-18 20:08:17 +0100

    Minimal Api Demo - Authorisation

--- MinimalApiDemo/Program.cs
+++ MinimalApiDemo/Program.cs
@@ -1,13 +1,49 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
+
+const string _issuer = "pp";
+const string _audience = "pp";
+const string _secret = "supersecretkey :)";
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JSON Web Token",
+    });
 
+    s.AddSecurityRequirement(new OpenApiSecurityRequirement() {{new OpenApiSecurityScheme {Reference = new OpenApiReference{Type = ReferenceType.SecurityScheme,Id = "Bearer"}},Array.Empty<string>()}});
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateActor = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = _issuer,
+        ValidAudience = _audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret))
+    };
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,29 +51,27 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/login", (string login, string password, HttpContext http) =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    //checking login & password - for demo open for all users :)
+    var claims = new[] {
+        new Claim(ClaimTypes.Actor, login),
+        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+    };
+
+    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secret));
+    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+    var tokenDescriptor = new JwtSecurityToken(_issuer, _audience, claims,
+        expires: DateTime.Now.Add(new TimeSpan(0, 5, 0)), signingCredentials: credentials);
+    return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
 })
-.WithName("GetWeatherForecast");
+.WithName("GetToken");
+
+
+app.MapGet("/dashboard", [Authorize] () => "Welcome to the secret Dashboard");
 
 app.Run();
-
-record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
