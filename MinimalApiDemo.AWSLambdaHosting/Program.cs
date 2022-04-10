3da556f601fc7caa51c34d3235a0ac133ce64057
piotrpenza <ppenza@gmail.com>
2022-04-10 12:03:33 +0200

    Minimal API hosted using AWS Lambda

---
+++ MinimalApiDemo.AWSLambdaHosting/Program.cs
@@ -0,0 +1,9 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello Lambda");
+
+app.Run();
