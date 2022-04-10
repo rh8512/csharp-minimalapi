7240f3d411d1547b01dc7cc6519cdf6fa5c5ef1b
Piotr Penza <ppenza@gmail.com>
2022-04-10 12:06:56 +0200

    Delete 6.MinimalApiDemo.AWSLambdaHosting directory

--- 6.MinimalApiDemo.AWSLambdaHosting/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
-
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello Lambda");
-
-app.Run();
